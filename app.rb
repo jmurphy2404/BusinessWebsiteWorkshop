9057d1d06ef9c87755dec109c72caeee8d3f709d
John Murphy <murf2073@gmail.com>
2017-10-10 11:07:48 -0400

    updated app.rb

--- app.rb
+++ app.rb
@@ -1,3 +1,15 @@
 require 'sinatra'
 require 'sendgrid-ruby'
 
+get '/' do
+  erb :home
+end
+get '/contact' do
+  erb :contact
+end  
+get '/locations' do
+  erb :locations
+end
+get '/gallery' do
+  erb :gallery
+end
